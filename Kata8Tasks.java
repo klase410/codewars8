8c900b7cfed14199a3703cd0f8e7f41ca33b1bb8
klase410 <46025585+klase410@users.noreply.github.com>
2023-12-13 09:55:25 +0200

    Add files via upload

---
+++ Kata8Tasks.java
@@ -0,0 +1,712 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Kata8Tasks {
+
+    // BEGIN / IB CS WarmUps
+
+    // https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/
+    // 8kyu Even or Odd
+    // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+    public static String evenOrOdd(int number) {
+        return ""; // Place code here;
+    }
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/50654ddff44f800200000004
+    // 8kyu Multiply
+    // This code does not execute properly. Try to figure out why.
+
+    public static Double multiply(Double a, Double b) {
+//        return a * b // fix me
+        return 0.0;
+    }
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/5715eaedb436cf5606000381
+    // 8kyu Sum of positive
+    // You get an array of numbers, return the sum of all of the positives ones.
+
+    public static int sum(int[] arr){
+        return 0;
+    }
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/55685cd7ad70877c23000102
+    // 8kyu Return Negative
+    // In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+    public static int makeNegative(final int x) {
+        return -1; // Your code here.
+    }
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/56dec885c54a926dcd001095
+    // 8kyu Opposite number
+    // Very simple, given an integer or a floating-point number, find its opposite.
+
+    public static int opposite(int number)
+    {
+        return -1; // your code here
+    }
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+    // 8kyu Reversed Strings
+    // Complete the solution so that it reverses the string passed into it.
+
+    public static String solution(String str) {
+        // Your code here...
+        return "";
+    }
+
+
+
+
+
+    // https://www.codewars.com/kata/53369039d7ab3ac506000467
+    // 8kyu Convert boolean values to strings 'Yes' or 'No'.
+    // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+    public static String boolToWord(boolean b)
+    {
+        return ""; //TODO
+    }
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+    // 8kyu Convert a Number to a String!
+    // We need a function that can transform a number (integer) into a string.
+
+    public static String numberToString(int num) {
+        return "";// Return a string of the number here!
+    }
+
+
+
+
+
+
+    // https://www.codewars.com/kata/544675c6f971f7399a000e79
+    // 8kyu Convert a String to a Number!
+    // We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+    public static int stringToNumber(String str) {
+        return -1; //TODO: Convert str into a number
+    }
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/551b4501ac0447318f0009cd
+    // 8kyu Convert a Boolean to a String
+    // Implement a function which convert the given boolean value into its string representation.
+
+    public static String convert(boolean b) {
+        return "";
+    }
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
+    // 8kyu Remove First and Last Character
+    // It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+    public static String remove(String str) {
+        return ""; // your code here
+    }
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/515e271a311df0350d00000f
+    // 8kyu Square(n) Sum
+    // Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+    public static int squareSum(int[] n)
+    {
+        return -1; //Your Code
+    }
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+    // 8kyu String repeat
+    // Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+    public static String repeatStr(final int repeat, final String string) {
+        return "";
+    }
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/55d24f55d7dd296eb9000030
+    // 8kyu Grasshopper - Summation
+    // Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+    public static int summation(int n) {
+        return 0;
+    }
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+    // 8kyu Remove String Spaces
+    // Write a function that removes the spaces from the string, then return the resultant string.
+
+    public static String noSpace(final String x) {
+        return x;
+    }
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/55a2d7ebe362935a210000b2
+    // 8kyu Find the smallest integer in the array
+    // Given an array of integers your solution should find the smallest integer.
+
+    public static int findSmallestInt(int[] args) {
+        return -1;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/54edbc7200b811e956000556
+    // 8kyu Counting sheep...
+    // Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+    public int countSheeps(Boolean[] arrayOfSheeps) {
+        return -1; // TODO May the force be with you
+    }
+
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/57356c55867b9b7a60000bd7
+    // 8kyu Basic Mathematical Operations
+    // Your task is to create a function that does four basic mathematical operations.
+
+    public static Integer basicMath(String op, int v1, int v2)
+    {
+        return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/582cb0224e56e068d800003c
+    // 8kyu Keep Hydrated!
+    // You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+    public int Liters(double time)  {
+        //Your code goes here! Hint: You should change that -1
+        return -1;
+    }
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
+    // 8kyu Century From Year
+    // The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+    public static int century(int number) {
+        return -1; // your code goes here
+    }
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3
+    // 8kyu Abbreviate a Two Word Name
+    // Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+    public static String abbrevName(String name) {
+        return name;
+    }
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/5583090cbe83f4fd8c000051
+    // !!! 8kyu Convert number to reversed array of digits
+    // Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+    public static int[] digitize(long n) {
+        return null; // Code here
+    }
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/5545f109004975ea66000086
+    // 8kyu Is n divisible by x and y?
+    // Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
+
+    public static boolean isDivisible(long n, long x, long y) {
+        return false; // your code
+    }
+
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/55a70521798b14d4750000a4
+    // 8kyu Returning Strings
+    // Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+    public static String greet(String name)
+    {
+        return ""; // Your code here
+    }
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
+    // 8kyu A Needle in the Haystack
+    // Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+    public static String findNeedle(Object[] haystack) {
+        return ""; // Your code here
+    }
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/555086d53eac039a2a000083
+    // 8kyu Opposites Attract
+    // Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+    public static boolean isLove(final int flower1, final int flower2) {
+        return false;
+    }
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/57f781872e3d8ca2a000007e
+    // 8kyu Beginner - Lost Without a Map
+    // Given an array of integers, return a new array with each value doubled.
+
+    public static int[] map(int[] arr) {
+        return null;
+    }
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd
+    // 8kyu Beginner Series #1 School Paperwork
+    // Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+    public static int paperWork(int n, int m)
+    {
+        return -1; //Happy Coding! ^_^
+    }
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a
+    // 8kyu Beginner Series #2 Clock
+    // Clock shows h hours, m minutes and s seconds after midnight. Your task is to write a function which returns the time since midnight in milliseconds.
+
+    public static int Past(int h, int m, int s)
+    {
+        return -1; //Happy Coding! ^_^
+    }
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/57a2013acf1fa5bfc4000921
+    // 8kyu Calculate average
+    // Write a function which calculates the average of the numbers in a given list.
+
+    public static double find_average(int[] array){
+        return 0.0;
+    }
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/53ee5429ba190077850011d4
+    // 8kyu You Can't Code Under Pressure #1
+    // Code as fast as you can! You need to double the integer and return it.
+
+    public static int doubleInteger(int i) {
+        // Double the integer and return it!
+        return i;
+    }
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/583710ccaa6717322c000105
+    // 8kyu Simple multiplication
+    // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+    public static int simpleMultiplication(int n) {
+        //your code here
+        return -1;
+    }
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad
+    // 8kyu Invert values
+    // Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+    public static int[] invert(int[] array) {
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/576bb71bbbcf0951d5000044
+    // 8kyu Count of positives / sum of negatives
+    // Given an array of integers. Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+    public static int[] countPositivesSumNegatives(int[] input)
+    {
+        return null; //return an array with count of positives and sum of negatives
+    }
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/57f780909f7e8e3183000078
+    // 8kyu Beginner - Reduce but Grow
+    // Given a non-empty array of integers, return the result of multiplying the values together in order.
+
+    public static int grow(int[] x){
+        return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/5601409514fc93442500010b
+    // 8kyu How good are you really?
+    // You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+    public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
+        return false; // Your code here
+    }
+
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/5556282156230d0e5e000089
+    // 8kyu DNA to RNA Conversion
+    // Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+    public String dnaToRna(String dna) {
+        return dna;  // Do your magic!
+    }
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+    // 8kyu Reversed sequence
+    // Build a function that returns an array of integers from n to 1 where n>0.
+
+    public static int[] reverse(int n){
+        //your code
+        return new int[]{};
+    }
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/5513795bd3fafb56c200049e
+    // 8kyu Count by X
+    // Create a function with two arguments that will return an array of the first n multiples of x.
+
+    public static int[] countBy(int x, int n){
+        // Your code here
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/55225023e1be1ec8bc000390
+    // 8kyu Jenny's secret message
+    // Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+
+    public static String greet_jenny(String name) {
+        return String.format("Hello, %s!", name);
+
+//        if(name.equals("Johnny")){
+//            return "Hello, my love!";
+//        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    // https://www.codewars.com/kata/5772da22b89313a4d50012f7
+    // 8kyu Grasshopper - Personalized Message
+    // Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+    static String greet(String name, String owner) {
+        return ""; // Add code here
+    }
+
+
+
+
+
+    // END
+
+    public static void main(String[] args) {
+        System.out.println(evenOrOdd(2));
+        System.out.println(multiply(1.5, 6.0));
+        System.out.println(sum(new int[]{-1,2,3,4,-5}));
+        System.out.println(makeNegative(50));
+        System.out.println(opposite(-4));
+        System.out.println(solution("world"));
+        System.out.println(boolToWord(false));
+        System.out.println(numberToString(100));
+        System.out.println(stringToNumber("500"));
+        System.out.println(convert(false));
+        System.out.println(remove("Hello, World"));
+        System.out.println(squareSum(new int[]{0, 6, 4, 6, 8, 2, -5}));
+        System.out.println(repeatStr(6, "Hello"));
+        System.out.println(summation(7));
+        System.out.println(noSpace("Hello, word !"));
+        System.out.println(findSmallestInt(new int[]{0, 6, 4, 6, 8, 2, -5}));
+        System.out.println(basicMath("+", 5, 7));
+        System.out.println(abbrevName("Hello World"));
+        System.out.println(find_average(new int[]{0, 6, 4, 6, 8, 2, -5}));
+        // ...
+        System.out.println(Arrays.toString(map(new int[]{0, 6, 4, 6, 8, 2, -5})));
+        System.out.println(Arrays.toString(countPositivesSumNegatives(new int[]{0, 6, 4, 6, 8, 2, -5})));
+        System.out.println(Arrays.toString(reverse(7)));
+        System.out.println(Arrays.toString(countBy(5, 10)));
+        System.out.println(Arrays.toString(digitize(2770149031L))); //
+    }
+}
